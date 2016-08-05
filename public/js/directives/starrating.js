5433b5856c3dc49dab321cbb9a9d1103ae2bf023
alec <abenson@wpi.edu>
2016-08-05 00:16:18 -0400

    add empty stars

--- public/js/directives/starrating.js
+++ public/js/directives/starrating.js
@@ -4,7 +4,8 @@ angular.module('souschef').directive('starRating', function () {
 	return {
 		link: link,
 		template: '<span ng-repeat="star in getFullStarCount(recipe.stars) track by $index"><i class="fa fa-star text-warning"></i></span>' +
-			'<span ng-repeat="star in getHalfStarCount(recipe.stars) track by $index"><i class="fa fa-star-half-o text-warning"></i></span>'
+			'<span ng-repeat="star in getHalfStarCount(recipe.stars) track by $index"><i class="fa fa-star-half-o text-warning"></i></span>' +
+			'<span ng-repeat="star in getEmptyStarCount(recipe.stars) track by $index"><i class="fa fa-star-o text-warning"></i></span>'
 	};
 
 	function link(scope) {
@@ -13,6 +14,10 @@ angular.module('souschef').directive('starRating', function () {
 		};
 		scope.getHalfStarCount = function (stars) {
 			var difference = Math.round(stars - Math.floor(stars));
+			return new Array(difference);
+		};
+		scope.getEmptyStarCount = function (stars) {
+			var difference = 5 - scope.getFullStarCount(stars).length - scope.getHalfStarCount(stars).length;
 			return new Array(difference);
 		};
 	}
