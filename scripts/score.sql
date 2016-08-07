74899f326bd047c5b9ca0aaa84ef1c03b1161c49
alec <abenson@wpi.edu>
2016-08-07 19:51:26 -0400

    it doesn't suck now

--- scripts/score.sql
+++ scripts/score.sql
@@ -1,33 +1,5 @@
 DELIMITER $$
 
-CREATE FUNCTION ingredient_matches(anchor_recipe_id INT, compare_recipe_id INT) RETURNS INT
-BEGIN
-  SELECT m.matches INTO @score FROM (
-    SELECT t2.recipe_id, SUM(CASE WHEN t1.name IS NOT NULL THEN 1 ELSE 0 END) AS matches
-      FROM ingredient_relations AS t1
-      RIGHT JOIN ingredient_relations AS t2
-      ON t1.name = t2.name
-      AND t1.recipe_id = anchor_recipe_id
-      WHERE t2.recipe_id <> anchor_recipe_id
-      GROUP BY t2.recipe_id
-    ) AS m WHERE m.recipe_id = compare_recipe_id;
-  RETURN @score;
-END$$
-
-CREATE FUNCTION maxMatchedIngredients(anchorId INT) RETURNS INT
-BEGIN
-  SELECT MAX(m.matches) INTO @maximum FROM (
-    SELECT t2.recipe_id, SUM(CASE WHEN t1.name IS NOT NULL THEN 1 ELSE 0 END) AS matches
-      FROM ingredient_relations AS t1
-      RIGHT JOIN ingredient_relations AS t2
-      ON t1.name = t2.name
-      AND t1.recipe_id = anchorId
-      WHERE t2.recipe_id <> anchorId
-      GROUP BY t2.recipe_id
-    ) AS m;
-  RETURN @maximum;
-END$$
-
 CREATE FUNCTION scoreWithoutAnchor(recipeId INT) RETURNS FLOAT
 BEGIN
   #Get max values in recipes table so we can normalize scoring
@@ -57,13 +29,31 @@ END$$
 
 CREATE FUNCTION scoreWithAnchor(anchorId INT, compareId INT) RETURNS FLOAT
 BEGIN
+  CREATE TEMPORARY TABLE IF NOT EXISTS recipeMatches
+  AS SELECT t2.recipe_id, SUM(CASE WHEN t1.name IS NOT NULL THEN 1 ELSE 0 END) AS matches
+    FROM ingredient_relations AS t1
+    RIGHT JOIN ingredient_relations AS t2
+    ON t1.name = t2.name
+    AND t1.recipe_id = anchorId
+    WHERE t2.recipe_id <> anchorId
+    GROUP BY t2.recipe_id
+    ORDER BY matches desc;
+
+  SELECT matches
+  INTO @ingredient_matches
+  FROM recipeMatches
+  WHERE recipe_id = compareId;
+
+  SELECT MAX(matches)
+  INTO @maxMatches
+  FROM recipeMatches;
+
   #Get efficiency preference
   SELECT r.efficiency
   INTO @prefs.efficiency
   FROM refinements as r;
-  set @maxMatches = maxMatchedIngredients(anchorId);
   set @priorityScore = scoreWithoutAnchor(compareId);
-  set @efficiencyScore = (ingredient_matches(anchorId, compareId) / @maxMatches) * @prefs.efficiency;
+  set @efficiencyScore = (@ingredient_matches / @maxMatches) * @prefs.efficiency;
   SET @score = @priorityScore + IFNULL(@efficiencyScore, 0);
   RETURN @score;
 END$$
